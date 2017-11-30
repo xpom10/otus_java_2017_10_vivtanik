c06652695888300e04c3ce2554e20ef6da99048a
xpom10 <xp10m@yandex.ru>
2017-12-01 00:05:12 +0300

    add Homework_06

--- Homework_06/src/main/java/ru/otus/Main.java
+++ Homework_06/src/main/java/ru/otus/Main.java
@@ -1,11 +1,13 @@
-package ru.otus.atm;
+package ru.otus;
+
+import ru.otus.atm.AtmImpl;
 
 public class Main {
 
     public static void main(String[] args) {
 
         AtmImpl atm = new AtmImpl(10, new int[]{10, 50, 100, 500, 1000, 5000});
-        System.out.println(atm.checkATMMoney());
+        atm.getMoney(660);
     }
 
 }
--- Homework_06/src/main/java/ru/otus/atm/AtmImpl.java
+++ Homework_06/src/main/java/ru/otus/atm/AtmImpl.java
@@ -7,7 +7,7 @@ public class AtmImpl {
     Map<Integer,Integer> cellsOfMoney;
 
     public AtmImpl(int numberOfBanknotes ,int[] nominal) {
-        this.cellsOfMoney = new HashMap<Integer, Integer>();
+        this.cellsOfMoney = new HashMap<>();
         for (Integer i : nominal) {
             cellsOfMoney.put(i,numberOfBanknotes);
         }
@@ -21,7 +21,21 @@ public class AtmImpl {
         return val;
     }
 
-    public void getMoney(int money) {
-
+    public void getMoney(Integer money) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = cellsOfMoney.keySet();
+        for (Integer i : set) {
+            list.add(i);
+        }
+        Collections.sort(list,Comparator.reverseOrder());
+        if (((money & cellsOfMoney.get(list.get(list.size() - 1))) == 0)) {
+            for (Integer a : list) {
+                if ((money / a) >=  1) {
+                    Integer newCount = cellsOfMoney.get(a) - (money/a);
+                    cellsOfMoney.put(a, newCount);
+                    money &= a;
+                }
+            }
+        }
     }
 }
